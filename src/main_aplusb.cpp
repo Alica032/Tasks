f0515e872e905965a25d5595edc168dba9cfc58c
Alica032 <alicadr.032@gmail.com>
2018-10-12 04:00:44 +0300

    add cpp files

--- src/main_aplusb.cpp
+++ src/main_aplusb.cpp
@@ -4,7 +4,7 @@
 #include <libgpu/context.h>
 #include <libgpu/shared_device_buffer.h>
 
-// Этот файл будет сгенерирован автоматически в момент сборки - см. convertIntoHeader в CMakeLists.txt:18
+// Этот файл будет сгенерирован автоматически в момент сборки - см. convertIntoHeader в CMakeLists.txt:22
 #include "cl/aplusb_cl.h"
 
 #include <vector>
@@ -26,7 +26,7 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 int main(int argc, char **argv)
 {
-    // TODO это пример использования обертки для решения предыдущего задания A+B
+    // Это пример использования библиотеки для решения предыдущего задания A+B
 
     // chooseGPUDevice:
     // - Если не доступо ни одного устройства - кинет ошибку
@@ -34,15 +34,17 @@ int main(int argc, char **argv)
     // - Если доступно N>1 устройства:
     //   - Если аргументов запуска нет или переданное число не находится в диапазоне от 0 до N-1 - кинет ошибку
     //   - Если аргумент запуска есть и он от 0 до N-1 - вернет устройство под указанным номером
+//            char *argvv[] = { "device", "0" };
+//            gpu::Device device = gpu::chooseGPUDevice(2, argvv);
     gpu::Device device = gpu::chooseGPUDevice(argc, argv);
-
     // Этот контекст после активации будет прозрачно использоваться при всех вызовах в libgpu библиотеке
-    // это достигается использованием thread-local переменных, т.е. на самом деле контекст будет активирован для текущего потока исполнения
+    // это достигается использованием thread-local переменных, т.е. на самом деле контекст будет активирован
+    // для текущего потока исполнения
     gpu::Context context;
     context.init(device.device_id_opencl);
     context.activate();
 
-    unsigned int n = 100*1000*1000;
+    unsigned int n = 50*1000*1000;
     std::vector<float> as(n, 0);
     std::vector<float> bs(n, 0);
     std::vector<float> cs(n, 0);
@@ -61,14 +63,16 @@ int main(int argc, char **argv)
 
     // Прогружаем данные из векторов as и bs
     // (есть нетипизированный метод write для которого количество измеряется в байтах,
-    // и типизированный writeN, для которого количество измеряется в количестве float-элементов, т.к. gpu::gpu_mem_32f - это shared_device_buffer_typed<float>)
+    // и типизированный writeN, для которого количество измеряется в количестве
+    // float-элементов, т.к. gpu::gpu_mem_32f - это shared_device_buffer_typed<float>)
     as_gpu.writeN(as.data(), n);
     bs_gpu.writeN(bs.data(), n);
 
     // Исходники кернела написаны в src/cl/aplusb.cl
     // Но благодаря convertIntoHeader(src/cl/aplusb.cl src/cl/aplusb_cl.h aplusb_kernel) (см. CMakeLists.txt:18)
     // при компиляции автоматически появится файл src/cl/aplusb_cl.h с массивом aplusb_kernel состоящим из байт исходника
-    // т.о. программе не будет нужно в runtime читать файл с диска, т.к. исходник кернелов теперь хранится в массиве данных основной программы
+    // т.о. программе не будет нужно в runtime читать файл с диска, т.к. исходник кернелов теперь хранится
+    // в массиве данных основной программы
     ocl::Kernel aplusb(aplusb_kernel, aplusb_kernel_length, "aplusb");
     aplusb.compile();
 
